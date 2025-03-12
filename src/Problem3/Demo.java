2914955c0aaa3aa0c6be4c990e2e9f57ab7d5764
Noah Lambe <noahalambe@gmail.com>
2025-03-12 18:44:20 -0230

    added two more classes to problem 3

---
+++ src/Problem3/Demo.java
@@ -0,0 +1,20 @@
+package Problem3;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[4];
+
+        shapes[0] = new Ellipse("Ellipse1", 8.0, 4.0);
+        shapes[1] = new Circle("Circle1", 5.0);
+        shapes[2] = new Triangle("Triangle1", 5.0, 3.0, 2.5);
+        shapes[3] = new EquilateralTriangle("Triangle2", 4.0, 4.0, 4.0);
+
+        for (Shape shape : shapes) {
+            System.out.println();
+            System.out.println(shape);
+            System.out.println(shape.getName() + " Area: " + String.format("%.2f", shape.getArea()));
+            System.out.println(shape.getName() + " Perimeter: " + String.format("%.2f", shape.getPerimeter()));
+        }
+    }
+}
---
+++ src/Problem3/Triangle.java
@@ -0,0 +1,67 @@
+package Problem3;
+
+public class Triangle extends Shape {
+    private String name;
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(String name, double side1, double side2, double side3) {
+        this.name = name;
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+
+        if (!(side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2)) {
+            throw new IllegalArgumentException("Not a valid triangle.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle [name=" + name + ", side1=" + side1 + ", side2=" + side2 + ", side3=" + side3 + "]";
+    }
+}
